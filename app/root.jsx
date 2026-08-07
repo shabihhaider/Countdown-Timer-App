f4b0ea0965deac76805b4ef1a903c50c613099b1
shabihhaider <2022cs723@student.uet.edu.pk>
2026-08-07 22:35:56 +0500

    fix: suppress hydration warnings from browser extensions (grammarly)

--- app/root.jsx
+++ app/root.jsx
@@ -11,7 +11,7 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body suppressHydrationWarning>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
